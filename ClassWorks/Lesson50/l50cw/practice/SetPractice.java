2e48cdbb6f12f4513fba234b24eb50fa43ec71a4
SancosJak <iskander.sancosjak@gmail.com>
2023-09-13 22:07:59 +0200

    update

--- ClassWorks/Lesson50/l50cw/practice/SetPractice.java
+++ ClassWorks/Lesson50/l50cw/practice/SetPractice.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class SetPractice {
     public static void main(String[] args) {
-
         //тк Set - интерфейс, для создания объекта мы используем конкретную реализацию, например HashSet
         // три основные реализации Set - представлены ниже. вы можете раскомментироавть любую, закомментировав две
         // другие, и посмотреть, что измениться
@@ -13,54 +12,54 @@ public class SetPractice {
 //        Set<String> set = new LinkedHashSet<>();
 
 
-        //добавление
-        set.add("one");
-        set.add("two");
-        set.add("three");
-        set.add("one");
-        set.add("two");
-        set.add("three");
-        //TreeSet не позволяет добавлять null, поэтому эту строку следует закомментировать, при выборе TreeSet
-        set.add(null);
-        // печать при помощи кастомного метода
-        print(set);
+            //добавление
+            set.add("one");
+            set.add("two");
+            set.add("three");
+            set.add("one");
+            set.add("two");
+            set.add("three");
+            //TreeSet не позволяет добавлять null, поэтому эту строку следует закомментировать, при выборе TreeSet
+            set.add(null);
+            // печать при помощи кастомного метода
+            print(set);
 
-        //.contains() - возвращает true или false в зависимости от того, содержится ли данный элемент в коллекции или нет
-        System.out.println(set.contains("one"));
+            //.contains() - возвращает true или false в зависимости от того, содержится ли данный элемент в коллекции или нет
+            System.out.println(set.contains("one"));
 
-        // ужаляет данный эелемент из коллекции.
-        set.remove("one");
+            // ужаляет данный эелемент из коллекции.
+            set.remove("one");
 
-        // возвращает количество элементов в коллекции
-        System.out.println(set.size());
+            // возвращает количество элементов в коллекции
+            System.out.println(set.size());
 
-        // .isEmpty() - возвращает true или false в зависимости от того, содержит ли даннаая колекция хоть что-то
-        System.out.println(set.isEmpty());
+            // .isEmpty() - возвращает true или false в зависимости от того, содержит ли даннаая колекция хоть что-то
+            System.out.println(set.isEmpty());
 
-        // удаляет все элементы в коллекции
-        set.clear();
-        System.out.println(set.size());
-        System.out.println(set.isEmpty());
+            // удаляет все элементы в коллекции
+            set.clear();
+            System.out.println(set.size());
+            System.out.println(set.isEmpty());
 
-        // в качестве альтернативы приведен ArrayList
-        ArrayList<String> list = new ArrayList<>();
-        list.add("one");
-        list.add("two");
-        list.add("three");
-        list.add("one");
-        list.add("two");
-        list.add("three");
-        print(list);
-
-
-        // при инициализации Set некими изначальными значениями мы получаем НЕИЗМЕНЯЕМЫЙ сет, то есть сет
-        // в который нельзя добавлять или удалять из него элементы
-        Set<String> immutable = Set.of("one", "two", "three");
-        //immutable.add("four");
-        //immutable.remove("one");
+            // в качестве альтернативы приведен ArrayList
+            ArrayList<String> list = new ArrayList<>();
+            list.add("one");
+            list.add("two");
+            list.add("three");
+            list.add("one");
+            list.add("two");
+            list.add("three");
+            print(list);
 
 
-    }
+            // при инициализации Set некими изначальными значениями мы получаем НЕИЗМЕНЯЕМЫЙ сет, то есть сет
+            // в который нельзя добавлять или удалять из него элементы
+            Set<String> immutable = Set.of("one", "two", "three");
+            //immutable.add("four");
+            //immutable.remove("one");
+
+
+        }
 
     //метод распечатает любой класс, который имплементирует интерфейс Iterable, то есть это может быть любая коллекция
     private static void print(Iterable iterable) {
@@ -73,14 +72,20 @@ public class SetPractice {
     // Задача1. Объединение множеств: Напишите метод, который принимает два множества и возвращает новое множество,
     // содержащее их объединение.
     public static Set<String> unionSet(Set<String> first, Set<String> second) {
-        //ваш код тут
-        return null;
+
+        Set<String> union = new HashSet<>(first);
+        union.addAll(second);
+
+        return union;
     }
 
     // Задача 2. Разность множеств: Создайте метод, который принимает два множества и возвращает новое множество,
     // содержащее только уникальные элементы из двух сетов (не совпадающие)
     public static Set<String> uniqueElement(Set<String> first, Set<String> second) {
-        //ваш код тут
-        return null;
+
+        Set<String> unique = new HashSet<>(first);
+        unique.removeAll(second);
+
+        return unique;
     }
 }
---
+++ ClassWorks/Lesson50/l50cw/practice/TeacherClasswork.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.HashSet;
+
+public class TeacherClasswork {
+    public static void main(String[] args) {
+        House h1 = new House(2, "123", 50);
+        House h2 = new House(2, "345", 50);
+
+
+        HashSet<House> set = new HashSet<>();
+        set.add(h1);
+        set.add(h2);
+
+        set.forEach(i -> System.out.println(i));
+        System.out.println(h1.equals(h2));
+    }
+
+}

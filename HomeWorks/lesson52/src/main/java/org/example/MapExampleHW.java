c655fe326b540c53780abab7be0ef41e1d625d12
SancosJak <iskander.sancosjak@gmail.com>
2023-09-18 19:26:22 +0200

    update

---
+++ HomeWorks/lesson52/src/main/java/org/example/MapExampleHW.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapExampleHW {
+    public static void main(String[] args) {
+        // pairs of "hero-power"
+        Map<String, Integer> heroPowers = new HashMap<>();
+
+        heroPowers.put("Spider-Man", 88);
+        heroPowers.put("Iron Man", 95);
+        heroPowers.put("Captain America", 72);
+
+        int spidermanPower = heroPowers.get("Spider-Man");
+        System.out.println("Spider-Man's power: " + spidermanPower);
+
+        if (heroPowers.containsKey("Iron Man")) {
+            System.out.println("Hero Iron Man is present.");
+        }
+
+        if (heroPowers.containsValue(72)) {
+            System.out.println("Power 72 is present.");
+        }
+
+        Set<String> heroNames = heroPowers.keySet();
+        System.out.println("Hero names: " + heroNames);
+
+        heroPowers.remove("Captain America");
+
+        System.out.println("Iterating over Map entries:");
+
+        for (Map.Entry<String, Integer> entry : heroPowers.entrySet()) {
+            String heroName = entry.getKey();
+            int power = entry.getValue();
+            System.out.println("Hero: " + heroName + ", Power: " + power);
+        }
+    }
+}

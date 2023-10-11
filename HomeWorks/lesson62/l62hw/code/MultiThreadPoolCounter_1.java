71364b2e86fffd2a4bbbfebf206b899e1931b117
SancosJak <iskander.sancosjak@gmail.com>
2023-10-11 21:59:13 +0200

    update

---
+++ HomeWorks/lesson62/l62hw/code/MultiThreadPoolCounter_1.java
@@ -0,0 +1,42 @@
+package code;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultiThreadPoolCounter_1 {
+    public static void main(String[] args) {
+
+        AtomicInteger counter = new AtomicInteger(0);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+
+        Runnable incrementTask = () -> {
+            for (int j = 0; j < 5000; j++) {
+                counter.incrementAndGet();
+            }
+        };
+
+        Runnable decrementTask = () -> {
+            for (int j = 0; j < 3000; j++) {
+                counter.decrementAndGet();
+            }
+        };
+
+        for (int i = 0; i < 3; i++) {
+            Future<?> future = executorService.submit(incrementTask);
+        }
+        for (int i = 0; i < 5; i++) {
+            Future<?> future = executorService.submit(decrementTask);
+        }
+
+        executorService.shutdown();
+
+        while (!executorService.isTerminated()) {
+
+        }
+
+        assert counter.get() == 0 : "Counter is not zero!";
+        System.out.println("Final Counter Value is: " + counter.get());
+    }
+}

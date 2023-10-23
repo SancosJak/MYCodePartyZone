0f160ccd2a3189cda30a42aab2725b415d404054
SancosJak <iskander.sancosjak@gmail.com>
2023-10-23 23:02:46 +0200

    update

--- CinemaSysHW/src/main/java/CinemaSysManagment/model/Administrator.java
+++ CinemaSysHW/src/main/java/CinemaSysManagment/model/Administrator.java
@@ -46,7 +46,6 @@ public class Administrator implements IAdministrator {
 
     @Override
     public IFilm createFilm(String name, int duration, String genre, String rating, String description, String language) {
-        // реализация создания фильма
         return new Film(name, duration, genre, rating, description, language);
     }
 
@@ -65,40 +64,33 @@ public class Administrator implements IAdministrator {
 
     @Override
     public ISession createSession(IFilm film, ICinemaHall cinemaHall, String startTime, String endTime) {
-        // реализация создания сеанса
         return new Session(film, cinemaHall, startTime, endTime);
     }
 
     @Override
     public IProduct createProduct(String name, double price, String description, int stockQuantity) {
-        // реализация создания продукта
         return new Product(name, price, description, stockQuantity);
     }
 
     @Override
     public void deleteFilm(IFilm film) {
-        // реализация удаления фильма
     }
 
     @Override
     public void deleteSession(ISession session) {
-        // реализация удаления сеанса
     }
 
     @Override
     public void deleteProduct(IProduct product) {
-        // реализация удаления продукта
     }
 
     @Override
     public String generateTicketSalesReport(String startDate, String endDate) {
-        //  реализация генерации отчета о продажах билетов
         return "Отчет о продажах билетов за указанный период времени";
     }
 
     @Override
     public String generateProductSalesReport(String startDate, String endDate) {
-        //  реализация генерации отчета о продажах продуктов
         return "Отчет о продажах продуктов за указанный период времени";
     }
 }
--- CinemaSysHW/src/main/java/CinemaSysManagment/model/Employee.java
+++ CinemaSysHW/src/main/java/CinemaSysManagment/model/Employee.java
@@ -14,7 +14,6 @@ public class Employee implements IEmployee {
     private String position;
     private List<ISession> managedSessions;
 
-    // Дополнительные свойства
     private String firstName;
     private String lastName;
     private String contactInfo;

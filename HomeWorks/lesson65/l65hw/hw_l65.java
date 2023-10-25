861c67900354933fcc104152f09d80a0eccb41c2
SancosJak <iskander.sancosjak@gmail.com>
2023-10-26 00:22:40 +0200

    update

---
+++ HomeWorks/lesson65/l65hw/hw_l65.java
@@ -0,0 +1,5 @@
+public class hw_l65 {
+    public static void main(String[] args) {
+        System.out.println("https://docs.google.com/forms/d/e/1FAIpQLSdXY2gHOWQ0A5nI4qWz_LhLJ7oMw0QXbTHTGcIlnldU8sIE7A/viewform?pli=1");
+    }
+}
